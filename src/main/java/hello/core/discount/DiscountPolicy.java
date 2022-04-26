ca44adabd437ba6e2001a9ffd80b1bd8bd96c221
soonpyo21 <tnsvy2004@gmail.com>
2022-04-26 23:44:55 +0900

    주문과 할인 도메인 실행과 테스트

--- src/main/java/hello/core/discount/DiscountPolicy.java
+++ src/main/java/hello/core/discount/DiscountPolicy.java
@@ -5,7 +5,6 @@ import hello.core.member.Member;
 public interface DiscountPolicy {
 
     /**
-     *
      * @return 할인 대상 금액
      */
     int discount(Member member, int price);
---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,25 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L; // Long 이 아닌 long으로 선언하면 값에 null을 넣을 수 없다.
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.calculatePrice()).isEqualTo(9000);
+
+    }
+}
