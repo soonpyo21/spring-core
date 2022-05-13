b3d97f0271106e4bd32e7bca0adcc7fd6ceae362
soonpyo21 <tnsvy2004@gmail.com>
2022-05-13 16:21:11 +0900

    컴포넌트 스캔과 의존관계 자동 주입 시작하기

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -36,10 +36,10 @@ public class ConfigurationSingletonTest {
     @Test
     void configurationDeep() {
         ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        //AppConfig도 스프링 빈으로 등록된다.
         AppConfig bean = ac.getBean(AppConfig.class);
 
         System.out.println("bean = " + bean.getClass());
-
-
+        //출력: bean = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$bd479d70
     }
 }
