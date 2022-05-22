bd9352607806d937be4a1a0419ceff5ab9629bc3
soonpyo21 <tnsvy2004@gmail.com>
2022-05-22 23:01:38 +0900

    자동, 수동의 올바른 실무 운영 기준

--- src/test/java/hello/core/autowired/AllBeanTest.java
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -50,4 +50,34 @@ public class AllBeanTest {
             return discountPolicy.discount(member, price);
         }
     }
+
+    // 자동, 수동의 올바른 실무 운영 기준
+
+    // 업무 로직 빈
+    // 업무 로직은 숫자도 매우 많고, 한번 개발해야 하면 컨트롤러, 서비스, 리포지토리 처럼 어느정도 유사한 패턴이 있다.
+    // 이런 경우 자동 기능을 적극 사용하는 것이 좋다. 보통 문제가 발생해도 어떤 곳에서 문제가 발생했는지 명확하게 파악하기 쉽다.
+
+    // 기술 지원 로직 빈
+    // 기술 지원 로직은 업무 로직과 비교해서 그 수가 매우 적고, 보통 애플리케이션 전반에 걸쳐서 광범위하게 영향을 미친다.
+    // 그리고 업무 로직은 문제가 발생했을 때 어디가 문제인지 명확하게 잘 들어나지만,
+    // 기술 지원 로직은 적용이 잘 되고 있는지 아닌지 조차 파악하기 어려운 경우가 많다.
+    // 그래서 이런 기술 지원 로직들은 가급적 수동 빈 등록을 사용해서 명확하게 들어내는 것이 좋다.
+
+    // 참고 - 비즈니스 로직 중에서 다형성을 적극 활용할 때
+
+//    @Configuration
+//    public class DiscountPolicyConfig {
+//        @Bean
+//        public DiscountPolicy rateDiscountPolicy() {
+//            return new RateDiscountPolicy();
+//        }
+//        @Bean
+//        public DiscountPolicy fixDiscountPolicy() {
+//            return new FixDiscountPolicy();
+//        }
+//    }
+
+    // 자동 등록이든 수동 등록이든간에 특정 패키지에 묶어두고,
+    // 위와 같이 한눈에 빈의 이름은 물론이고 어떤 빈들이 주입될지 파악할 수 있게 설계하는 것이 좋다.
+
 }
